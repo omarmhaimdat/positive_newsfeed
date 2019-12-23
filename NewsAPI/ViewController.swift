7bd6b09f2196272d5db955a2186954ed1d96750b
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-12-23 22:05:16 +0100

    Initial Commit

---
+++ NewsAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by M'haimdat omar on 23-12-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
